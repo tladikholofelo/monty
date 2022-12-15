a383dbf116113ba1342b62398f42039ad667fd67
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-12-14 18:53:59 -0800

    Add div.c

---
+++ div.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * f_div - divides the second top element of the stack
+ * by the top element of the stack
+ * @head: head of the stack
+ * @counter: line count
+ */
+void f_div(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int length = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	temp = h->next->n / h->n;
+	h->next->n = temp;
+	*head = h->next;
+	free(h);
+}
