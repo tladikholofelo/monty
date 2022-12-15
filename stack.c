f5360fa6aaaab92a7621f434177539a376db964e
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-12-14 19:00:30 -0800

    Add stack.c and queue.c

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_stack - sets the format of the data to a stack (LIFO)
+ * @head: head of the stack
+ * @counter: line count
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
