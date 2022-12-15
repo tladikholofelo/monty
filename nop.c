89a6a449ea9437940a60bd98b50efdb6cc930dce
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2022-12-14 18:51:15 -0800

    Add nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * f_nop - doesn’t do anything
+ * @head: head of the stack
+ * @counter: line count
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
